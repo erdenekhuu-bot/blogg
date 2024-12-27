e8cbde6a8ab3ff6e180311620949a2932aac8cd7
unknown <hydra2medusa@gmail.com>
2024-12-28 00:05:10 +0800

    '.'

--- iblogger/src/App.tsx
+++ iblogger/src/App.tsx
@@ -1,36 +1,49 @@
-import { createContext } from "react";
+import { createContext, ReactNode } from "react";
 import { Layout, Flex } from "antd";
-import Headers from "./pages/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Headers from "./components/Header";
 import logo_slogan from "../public/assets/logo_slogan.svg";
+import Home from "./pages/Home";
 
 const RootLayout = createContext("");
 const { Header, Footer, Content } = Layout;
-function App() {
+
+function App({ children }: { children: ReactNode }) {
   return (
     <RootLayout.Provider value="">
-      <Layout>
-        <Header className="bg-white">
-          <Headers />
-        </Header>
-        <Content>Content</Content>
-        <Footer className="border bg-white">
-          <Flex align="center" justify="space-around">
-            <img src={logo_slogan} alt="" />
-            <div>
-              <p className="text-lg font-bold">Тусламж</p>
-              <p>Хэрэглэх заавар</p>
-              <p>Санал хүсэлт</p>
-            </div>
-            <section>
-              <p className="text-lg font-bold">Бидэнтэй холбогдох</p>
-              <p>info@jstemplate.net</p>
-              <p>+976 99112344</p>
-            </section>
-          </Flex>
-        </Footer>
-      </Layout>
+      <BrowserRouter>
+        <Layout>
+          <Header className="bg-white">
+            <Headers />
+          </Header>
+          <Content>{children}</Content>
+          <Footer className="border bg-white">
+            <Flex align="center" justify="space-around">
+              <img src={logo_slogan} alt="" />
+              <div>
+                <p className="text-lg font-bold">Тусламж</p>
+                <p>Хэрэглэх заавар</p>
+                <p>Санал хүсэлт</p>
+              </div>
+              <section>
+                <p className="text-lg font-bold">Бидэнтэй холбогдох</p>
+                <p>info@jstemplate.net</p>
+                <p>+976 99112344</p>
+              </section>
+            </Flex>
+          </Footer>
+        </Layout>
+      </BrowserRouter>
     </RootLayout.Provider>
   );
 }
 
-export default App;
+export default function AppRoutes() {
+  return (
+    <App>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </App>
+  );
+}
---
+++ iblogger/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import no_post from "../../public/assets/no_posts.svg";
+
+export default function Home() {
+  return (
+    <div>
+      <img src={no_post} alt="" />
+    </div>
+  );
+}
