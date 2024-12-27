5e95a54b035bdf24c9f174f4472839cb59abfe79
unknown <hydra2medusa@gmail.com>
2024-12-28 00:22:16 +0800

    '.'

--- iblogger/src/App.tsx
+++ iblogger/src/App.tsx
@@ -5,6 +5,8 @@ import Headers from "./components/Header";
 import logo_slogan from "../public/assets/logo_slogan.svg";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const RootLayout = createContext("");
 const { Header, Footer, Content } = Layout;
@@ -12,40 +14,42 @@ const { Header, Footer, Content } = Layout;
 function App({ children }: { children: ReactNode }) {
   return (
     <RootLayout.Provider value="">
-      <BrowserRouter>
-        <Layout>
-          <Header className="bg-white">
-            <Headers />
-          </Header>
-          <Content>{children}</Content>
-          <Footer className="border bg-white">
-            <Flex align="center" justify="space-around">
-              <img src={logo_slogan} alt="" />
-              <div>
-                <p className="text-lg font-bold">Тусламж</p>
-                <p>Хэрэглэх заавар</p>
-                <p>Санал хүсэлт</p>
-              </div>
-              <section>
-                <p className="text-lg font-bold">Бидэнтэй холбогдох</p>
-                <p>info@jstemplate.net</p>
-                <p>+976 99112344</p>
-              </section>
-            </Flex>
-          </Footer>
-        </Layout>
-      </BrowserRouter>
+      <Layout>
+        <Header className="bg-white">
+          <Headers />
+        </Header>
+        <Content>{children}</Content>
+        <Footer className="border bg-white">
+          <Flex align="center" justify="space-around">
+            <img src={logo_slogan} alt="" />
+            <div>
+              <p className="text-lg font-bold">Тусламж</p>
+              <p>Хэрэглэх заавар</p>
+              <p>Санал хүсэлт</p>
+            </div>
+            <section>
+              <p className="text-lg font-bold">Бидэнтэй холбогдох</p>
+              <p>info@jstemplate.net</p>
+              <p>+976 99112344</p>
+            </section>
+          </Flex>
+        </Footer>
+      </Layout>
     </RootLayout.Provider>
   );
 }
 
 export default function AppRoutes() {
   return (
-    <App>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </App>
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Register />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
   );
 }
