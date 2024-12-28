7cdbdab664ecc94c239e98f33aded457ff8d1625
unknown <hydra2medusa@gmail.com>
2024-12-29 02:10:35 +0800

    '.'

--- iblogger/src/components/Header.tsx
+++ iblogger/src/components/Header.tsx
@@ -1,18 +1,54 @@
-import { Flex, Dropdown } from "antd";
-import logo from "../assets/logo.svg";
-import { useState } from "react";
+import { Button, Input, Space, Dropdown, Modal } from "antd";
 import {
   SearchOutlined,
   CommentOutlined,
   UserAddOutlined,
 } from "@ant-design/icons";
-import { Button, Input, Space } from "antd";
+import { useNavigate } from "react-router-dom";
 import heart from "../assets/u_heart.svg";
-import { items } from "../data/drop-down-data";
+import { useDropdownItems } from "../data/drop-down-data";
+import logo from "../assets/logo.svg";
+import { Flex } from "antd";
+import { useState } from "react";
+
 const Headers = function () {
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState("Content of the modal");
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleOk = () => {
+    setModalText("The modal will be closed after two seconds");
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setOpen(false);
+  };
+
+  const items = useDropdownItems(showModal);
+
   return (
     <Flex align="end" justify="space-around">
-      <img src={logo} alt="" width={91.62} height={46} />
+      <img
+        src={logo}
+        alt="Logo"
+        width={91.62}
+        height={46}
+        className="hover:cursor-pointer"
+        onClick={() => {
+          navigate("/");
+        }}
+      />
       <Space.Compact size="large" style={{ width: "40%" }}>
         <Input
           addonAfter={<SearchOutlined />}
@@ -21,18 +57,34 @@ const Headers = function () {
         />
       </Space.Compact>
       <Flex gap={10}>
-        <img src={heart} alt="" />
+        <img src={heart} alt="Heart" />
         <Dropdown menu={{ items }}>
           <Button type="primary" size="large" className="bg-[#E86B02]">
             Post
             <CommentOutlined />
           </Button>
         </Dropdown>
-        <Button block size="large">
+        <Modal
+          title="Title"
+          open={open}
+          onOk={handleOk}
+          confirmLoading={confirmLoading}
+          onCancel={handleCancel}
+        >
+          {modalText}
+        </Modal>
+        <Button
+          block
+          size="large"
+          onClick={() => {
+            navigate("/profile");
+          }}
+        >
           Профайл <UserAddOutlined />
         </Button>
       </Flex>
     </Flex>
   );
 };
+
 export default Headers;
--- iblogger/src/data/drop-down-data.tsx
+++ iblogger/src/data/drop-down-data.tsx
@@ -1,7 +1,36 @@
-import { HeartOutlined, UserOutlined, LogoutOutlined } from "@ant-design/icons";
+// import { HeartOutlined, UserOutlined, LogoutOutlined } from "@ant-design/icons";
 
-export const items = [
-  { key: 1, label: "Миний мэдээлэл", icon: <UserOutlined /> },
-  { key: 2, label: "Таалагдсан", icon: <HeartOutlined /> },
-  { key: 3, label: "Гарах", icon: <LogoutOutlined /> },
-];
+// export const items = [
+//   { key: 1, label: "Миний мэдээлэл", icon: <UserOutlined /> },
+//   { key: 2, label: "Таалагдсан", icon: <HeartOutlined /> },
+//   { key: 3, label: "Гарах", icon: <LogoutOutlined />, onclick: () => {} },
+// ];
+
+import { HeartOutlined, UserOutlined, LogoutOutlined } from "@ant-design/icons";
+import { MenuProps } from "antd";
+import { useNavigate } from "react-router-dom";
+
+export const useDropdownItems = (showModal: () => void): MenuProps["items"] => {
+  const navigate = useNavigate();
+
+  return [
+    {
+      key: "1",
+      label: "Миний мэдээлэл",
+      icon: <UserOutlined />,
+      onClick: () => navigate("/profile"),
+    },
+    {
+      key: "2",
+      label: "Таалагдсан",
+      icon: <HeartOutlined />,
+      onClick: () => navigate("/favorites"),
+    },
+    {
+      key: "3",
+      label: "Гарах",
+      icon: <LogoutOutlined />,
+      onClick: showModal, // Open modal on click
+    },
+  ];
+};
